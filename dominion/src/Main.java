86b93bad1af4031cdc952dc1775070d20eda2716
Sinner96 <abdallah.y.elgendi@fau.de>
2024-12-05 16:14:33 +0100

    checkwinner() stil giltched  & choosetarget()npc move is fast and errored when reached max

--- dominion/src/Main.java
+++ dominion/src/Main.java
@@ -3,7 +3,7 @@ import gdi.game.dominion.DominionInterface;
 
 public class Main {
     public static void main(String[] args) {
-        DominionTileManager board = new DominionTileManager(5,7);
+        DominionTileManager board = new DominionTileManager(4,4);
         Dominion dom = new Dominion(board);
 
         BaseGame game = new BaseGame(args, dom);
